44d18ad452a59cd57bf94a04c4537afbaead8479
Chandusvnit <harkeshkumar836@gmail.com>
2025-10-05 12:27:41 +0530

    Remove node_modules from tracking

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Start with backend ");
+import dotenv from "dotenv";
+
+dotenv.config({
+    path : "./.env",
+});
+
+let myname = process.env.database;
+console.log("Value : ,"  , myname);
+
+console.log("Start with backend new project ");
